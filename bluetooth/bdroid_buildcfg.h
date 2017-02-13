93519e87798acd78ae7a66dd5ea15f2f4326e1ec
Tarun93 <tarun.kapadia@micromaxinfo.com>
2017-02-13 16:29:37 +0530

    bluetooth: default name YU5050
    Signed-off-by: Tarun93 <tarun.kapadia@micromaxinfo.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   ""
+#define BTM_DEF_LOCAL_NAME   "YU5050"
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS    7
